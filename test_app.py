64ecccb0e3d711daf959e5886c2aaf462faac5cd
michaelmil2021 <michaelmil2021@gmail.com>
2025-02-04 23:19:54 +0000

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,14 +1,17 @@
 import pytest
 from app import app
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
+
 def test_greetme(client):
     response = client.get('/')
-    assert response.data==b'hello devops'
-    assert response.status_code==200
+    assert response.get_data(as_text=True) == 'hello devops'
+    assert response.status_code == 200
+
 def test_page(client):
     response = client.get('/page1')
-    assert response.data==b'welcome to page1'
-    assert response.status_code==200
+    assert response.get_data(as_text=True) == 'welcome to page1'
+    assert response.status_code == 200
